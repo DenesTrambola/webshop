9226cdca775d0294ce6adf82d76a25581857a1fb
DenesTrambola <156126997+DenesTrambola@users.noreply.github.com>
2025-02-04 14:00:04 +0200

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+//     <Routes>
+
+//         <Route path="/" element={ <Layout /> }>
+//           <Route index element={<Home />}/>
+
+//           <Route path="about" element={<About />}>
+//             <Route index element={<Teams />} />
+//             <Route path="teams" element={<Teams />} />
+//             <Route path="company-info" element={<CompanyInfo />} />
+//           </Route>
+
+//           <Route path="blog" element={<Blog />}/>
+//           <Route path="blog/:id" element={<Single />}/>
+//           <Route path="blog/new" element={<CreatePost />}/>
+
+//           <Route path='login' element={<Login />} />
+
+//           <Route path="*" element={<NotFound404 />}/>
+//         </Route>
+
+//     </Routes>
+
+    <>
+      <h1>PROOOM</h1>
+    </>
+
+  )
+}
+
+export default App
