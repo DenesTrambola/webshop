4e57dddf2f74ea59d1648fa80ce96dd22c17b891
DenesTrambola <156126997+DenesTrambola@users.noreply.github.com>
2025-02-14 23:50:11 +0200

    feature: implement login and register system

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from './Home'
 export { default as NotFound } from './NotFound';
 export { default as Category } from './Category';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
