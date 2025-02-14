ff4d5dfd21426ee5f4caa6c5fa3dc7a4771fa9a8
DenesTrambola <156126997+DenesTrambola@users.noreply.github.com>
2025-02-14 13:54:40 +0200

    feature: navigate to Home page with logo

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,14 +3,22 @@ import HeaderLogo from '../assets/img/header/header-logo.webp';
 import SearchBar from '../components/header/SearchBar';
 import Buttons from '../components/header/Buttons';
 import SignInSection from '../components/header/SignInSection';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const navigateToHome = () => {
+    if (location.pathname !== '/') navigate('/');
+  };
+
   return (
     <>
       <a><div id="top"><img src={TopImg} alt="Find out where your order is" loading="lazy" /></div></a>
 
       <header>
-        <a><img src={HeaderLogo} id='header-logo' loading='lazy' /></a>
+        <a onClick={navigateToHome}><img src={HeaderLogo} id='header-logo' loading='lazy' /></a>
 
         <SearchBar />
         <Buttons />
